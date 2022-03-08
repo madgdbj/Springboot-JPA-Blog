807cc0e082317ee4d05bdeeab8fa020fa9c0685f
madgdbj <92001682+madgdbj@users.noreply.github.com>
2022-03-08 09:08:34 +0900

    Add files via upload

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,49 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+	
+	
+	@Id //기본킨
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+	private int id;
+	
+	@Column(nullable = false, length = 30) 
+	private String username; 
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	
+	@Column(nullable = false, length = 50)
+	private String email; 
+
+	@ColumnDefault("'user'")
+	private String role;
+	
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+}
